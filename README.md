4a6f9a2366f592d1cdd15fac3675adda36086ea8
Roberto Duran <rduranwatson@tulane.edu>
2023-02-11 19:45:27 +0000

    done

--- README.md
+++ README.md
@@ -47,13 +47,65 @@ where $W(1) = 1$.
 - [ ] 3. (2 point) Now implement `work_calc`, which generalizes the above so that we can now input $a$, $b$ and a *function* $f(n)$ as arguments. Test this code by completing the test cases in `test_work` and adding 3 more cases.
 
 - [ ] 4. (2 point) Now, derive the asymptotic behavior of $W(n)$ using $f(n) = 1$, $f(n) = \log n$ and $f(n) = n$. Then, generate actual values for $W(n)$ for your code and confirm that the trends match your derivations.
+    - $f(n) = 1$: 
+    - $W(n) = aW(n/b) + f(x)$
+    - $W(n) = 2W(n/2) + 1$ 
+    - $W(n) = 2W(n/2) + k$
+    - $n * W(1) + (n-1)k$
+    - $2nk - k$
+    - $O(n)$
+      - When $n = 8, a = 2, b = 2,$ and $f(x) = 1$ 
+        - $W(n) = 15$
+  - $f(n) = n$:
+    - $W(n) = aW(n/b) + f(x)$
+    - $W(n) = 2W(n/2) + n$
+    - $n + 2 (n/2 + 2W(n/2^{2}))$
+    - $n + 2n/2 + 2^{2}W(n/2^{2})$
+    - $n + 2n + 2^{2}n + 2^{3}n...$
+    - $O(n^{2})$
+      - When $n = 8, a = 2, b = 2,$ and $f(x) = n$
+        - $W(n) = 32$
 
-**TODO: your answer goes here**
+  - $f(n) = n^{2}$: 
+    - $W(n) = aW(n/b) + f(x)$
+    - $W(n) = 2W(n/2) + n^{2}$
+    - $log_{b}^{a} = log_{2}(2)$
+    - $O(n^{2})$
+      - When $n = 8, a = 2, b = 2,$ and $f(x) = n^{2}$ 
+        - $W(n) = 30$
+
+|     n |   W_1 |   W_2 |
+|-------|-------|-------|
+|    10 |     4 |    18 |
+|    20 |     5 |    38 |
+|    50 |     6 |    97 |
+|   100 |     7 |   197 |
+|  1000 |    10 |  1994 |
+|  5000 |    13 |  9995 |
+| 10000 |    14 | 19995 |
+<br>
 
 - [ ] 5. (4 points) Now that you have a nice way to empirically generate valuess of $W(n)$, we can look at the relationship between $a$, $b$, and $f(n)$. Suppose that $f(n) = n^c$. What is the asypmptotic behavior of $W(n)$ if $c < \log_b a$? What about $c > \log_b a$? And if they are equal? Modify `compare_work` to compare empirical values for different work functions (at several different values of $n$) to justify your answer. 
 
-**TODO: your answer goes here**
+  - For c < log a W(n) grows slower than f(n)
+  - For c > log a W(n) grows faster than f(n)
+  - For c = log a W(n) grows at the same rate as f(n)
+
+|     n |       W_1 |           W_2 |
+|-------|-----------|---------------|
+|    10 |       100 |          1000 |
+|    20 |       400 |          8000 |
+|    50 |      2500 |        125000 |
+|   100 |     10000 |       1000000 |
+|  1000 |   1000000 |    1000000000 |
+|  5000 |  25000000 |  125000000000 |
+| 10000 | 100000000 | 1000000000000 |
 
 - [ ] 6. (3 points) $W(n)$ is meant to represent the running time of some recursive algorithm. Suppose we always had $a$ processors available to us and we wanted to compute the span of the same algorithm. Implement the function `span_calc` to compute the empirical span, where the work of the algorithm is given by $W(n)$. Implement `test_compare_span` to create a new comparison function for comparing span functions. Derive the asymptotic expressions for the span of the recurrences you used in problem 4 above. Confirm that everything matches up as it should. 
 
-**TODO: your answer goes here**
+  - $f(n) = 1$
+      - $W(n) = O(log^2 n)$
+  - $f(n) = log n$
+      - $W(n) = O(log n)$
+  - $f(n) = n$
+      - $W(n) = O(n)$
--- main.py
+++ main.py
@@ -2,13 +2,12 @@
 CMPS 2200  Recitation 2
 """
 
-### the only imports needed are here
 import tabulate
 import time
-###
+
 
 def simple_work_calc(n, a, b):
-    """Compute the value of the recurrence $W(n) = aW(n/b) + n
+  """Compute the value of the recurrence $W(n) = aW(n/b) + n
 
     Params:
     n......input integer
@@ -17,20 +16,25 @@ def simple_work_calc(n, a, b):
 
     Returns: the value of W(n).
     """
-    if n > 1:
-        return a * simple_work_calc(n//b, a, b) + n
-    else:
-        return n
-    pass
+  if n > 1:
+    return a * simple_work_calc(n // b, a, b) + n
+  else:
+    return n
+  pass
+
 
 def test_simple_work():
-    """ done. """
-    assert work_calc(10, 2, 2) == 2
-    assert work_calc(20, 3, 2) == 3
-    assert work_calc(30, 4, 2) == 1
+  """ done. """
+  assert simple_work_calc(10, 2, 2) == 36
+  assert simple_work_calc(20, 3, 2) == 230
+  assert simple_work_calc(30, 4, 2) == 650
+  assert simple_work_calc(200, 2, 2) == 1504
+  assert simple_work_calc(1000, 200, 500) == 1400
+  assert simple_work_calc(300, 70, 46) == 720
+
 
 def work_calc(n, a, b, f):
-    """Compute the value of the recurrence $W(n) = aW(n/b) + f(n)
+  """Compute the value of the recurrence $W(n) = aW(n/b) + f(n)
 
     Params:
     n......input integer
@@ -41,14 +45,15 @@ def work_calc(n, a, b, f):
 
     Returns: the value of W(n).
     """
-    if n > 1:
-        return a * work_calc(n//b, a, b, f) + f(n)
-    else:
-        return 1
-    pass
+  if n > 1:
+    return a * work_calc(n // b, a, b, f) + f(n)
+  else:
+    return 1
+  pass
+
 
 def span_calc(n, a, b, f):
-    """Compute the span associated with the recurrence $W(n) = aW(n/b) + f(n)
+  """Compute the span associated with the recurrence $W(n) = aW(n/b) + f(n)
 
     Params:
     n......input integer
@@ -59,20 +64,27 @@ def span_calc(n, a, b, f):
 
     Returns: the value of W(n).
     """
-    if n > 1:
-        return span_calc(n//b, a, b, f) + f(n)
-    else:
-        return 1
-    pass
+  if n > 1:
+    return span_calc(n // b, a, b, f) + f(n)
+  else:
+    return 1
+  pass
+
 
 def test_work():
-    """ done. """
-    assert work_calc(10, 2, 2,lambda n: 1) == 1
-    assert work_calc(20, 1, 2, lambda n: n*n) == 2
-    assert work_calc(30, 3, 2, lambda n: n) == 3
+  """ done. """
+  assert work_calc(10, 2, 2, lambda n: 1) == 15
+  assert work_calc(20, 1, 2, lambda n: n * n) == 530
+  assert work_calc(30, 3, 2, lambda n: n) == 300
+  assert work_calc(100, 45, 6, lambda n: 1) == 93196
+  assert work_calc(50, 35, 22, lambda n: n) == 1345
+  assert work_calc(75, 256, 145, lambda n: n) == 331
 
-def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
-    """
+
+def compare_work(work_fn1,
+                 work_fn2,
+                 sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
+  """
     Compare the values of different recurrences for 
     given input sizes.
 
@@ -81,94 +93,53 @@ def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000])
     (n, work_fn1(n), work_fn2(n), ...)
     
     """
-    result = []
-    for n in sizes:
-        # compute W(n) using current a, b, f
-        result.append((
-            n,
-            work_fn1(n),
-            work_fn2(n)
-            ))
-    return result
+  result = []
+  for n in sizes:
+    # compute W(n) using current a, b, f
+    result.append((n, work_fn1(n), work_fn2(n)))
+  return result
+
 
 def print_results(results):
-    """ done """
-    print(tabulate.tabulate(results,
-                            headers=['n', 'W_1', 'W_2'],
-                            floatfmt=".3f",
-                            tablefmt="github"))
+  """ done """
+  print(
+    tabulate.tabulate(results,
+                      headers=['n', 'W_1', 'W_2'],
+                      floatfmt=".3f",
+                      tablefmt="github"))
 
 
 def test_compare_work():
-    # curry work_calc to create multiple work
-    # functions taht can be passed to compare_work
-    
-    # create work_fn1
-    # create work_fn2
-    
-    def work_fn1(n):
-        def x(a):
-            def y(b):
-                def z(f):
-                    return work_calc(n, a, b, f)
-                return z
-            return y
-        return x
-    
-    def work_fn2(n):
-        def x(a):
-            def y(b):
-                def z(f):
-                    return work_calc(n, a, b, f)
-                return z
-            return y
-        return x
+  # curry work_calc to create multiple work
+  # functions taht can be passed to compare_work
 
-    res = compare_work(work_fn1, work_fn2)
-    print(res)
+  # create work_fn1
+  # create work_fn2
+
+  def work_fn1(n):
+    return n**2
+
+  def work_fn2(n):
+    return n**3
+
+  res = compare_work(work_fn1, work_fn2)
+  print_results(res)
+
 
 def test_compare_span():
-    assert span_calc(10, 2, 2, lambda n: 1) == 4
-    assert span_calc(20, 1, 2, lambda n: n*n) == 426
-    assert span_calc(30, 3, 2, lambda n: n) == 28
-    def span_fn1(n):
-        def x(a):
-            def y(b):
-                def z(f):
-                    return span_calc(n, a, b, f)
-                return z
-            return y
-        return x
-    
-    def span_fn2(n):
-        def x(a):
-            def y(b):
-                def z(f):
-                    return span_calc(n, a, b, f)
-                return z
-            return y
-        return x
-    
-    def compare_span(span_fn1, span_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
-        """
-        Compare the values of different recurrences for 
-        given input sizes.
+  assert span_calc(10, 2, 2, lambda n: 1) == 4
+  assert span_calc(20, 1, 2, lambda n: n * n) == 530
+  assert span_calc(30, 3, 2, lambda n: n) == 56
 
-        Returns:
-        A list of tuples of the form
-        (n, work_fn1(n), work_fn2(n), ...)
-        
-        """
-        result = []
-        for n in sizes:
-            # compute W(n) using current a, b, f
-            result.append((
-                n,
-                span_fn1(n),
-                span_fn2(n)
-                ))
-        return result
-    
-    res = compare_span(span_fn1, span_fn2)
-    print_results(res)
-    
+  def span_fn1(n):
+    return span_calc(n, 2, 2, lambda n: 1)
+
+  def span_fn2(n):
+    return span_calc(n, 2, 2, lambda n: n)
+
+  res = compare_work(span_fn1, span_fn2)
+  print_results(res)
+
+
+test_compare_span()
+test_compare_work()
